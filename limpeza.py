242f78c10b5b951a1da5d1f9ff71ccd19f8384af
Caio <caiocesar0052@gmail.com>
2026-05-20 15:44:20 -0300

    Correção e Conversão de Nulos

--- limpeza.py
+++ limpeza.py
@@ -10,11 +10,19 @@ df = df.drop_duplicates(subset='id_pedido', keep='first') # Remove duplicatas
 df['quantidade'] = pd.to_numeric(df['quantidade'], errors='coerce').astype('Int64') # Quantidade de Float para Int
 df['data_pedido'] = pd.to_datetime(df['data_pedido'], format='mixed', errors='coerce') # Convertendo Data de STR pra Date e ordenando
 
+#Correção Valores Nulos
+df['cliente_id'] = df['cliente_id'].fillna('CLI-N/I')
+df['email'] = df['email'].fillna('Email não informado')
+df['avaliacao'] = df['avaliacao'].fillna(0)
+
+#Correção nos dados
+df['nome_cliente'] = df['nome_cliente'].str.title()
+df['estado'] = (
+    df['estado']
+    .str.upper()
+    .str.replace(r'^S.*', 'SP', regex=True)
+    .str.replace('RIO', 'RJ', regex=True)
+)
 
 print(df.dtypes)
-print(df.head(10))
-
-
-
-
-
+print(df.tail(20))
