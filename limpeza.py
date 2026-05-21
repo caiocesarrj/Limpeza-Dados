01a7814d0e1cbd5dc4cc9fa8eeba7b726e1ce78c
Caio <caiocesar0052@gmail.com>
2026-05-21 15:12:18 -0300

    Salvando estrutura 80% Concluída sobre limpeza

--- limpeza.py
+++ limpeza.py
@@ -10,19 +10,31 @@ df = df.drop_duplicates(subset='id_pedido', keep='first') # Remove duplicatas
 df['quantidade'] = pd.to_numeric(df['quantidade'], errors='coerce').astype('Int64') # Quantidade de Float para Int
 df['data_pedido'] = pd.to_datetime(df['data_pedido'], format='mixed', errors='coerce') # Convertendo Data de STR pra Date e ordenando
 
-#Correção Valores Nulos
+#Correção e Remoção de Valores Nulos
 df['cliente_id'] = df['cliente_id'].fillna('CLI-N/I')
 df['email'] = df['email'].fillna('Email não informado')
 df['avaliacao'] = df['avaliacao'].fillna(0)
+df = df.dropna(subset=['quantidade', 'preco_unitario']) #Removendo quantidades e precos nulos, não tem valor de receita
 
 #Correção nos dados
-df['nome_cliente'] = df['nome_cliente'].str.title()
+df['nome_cliente'] = df['nome_cliente'].str.title() # Padronizando nomes
 df['estado'] = (
     df['estado']
     .str.upper()
     .str.replace(r'^S.*', 'SP', regex=True)
     .str.replace('RIO', 'RJ', regex=True)
-)
+)                                            #Padronizando Estados
+df['status'] = df['status'].str.upper()
+
+df.loc[df['desconto'] > 1, 'desconto'] = np.nan # Descontos acima de 100% retornam desconhecido
+
+
+#Receita de pedidos entregues, devolvidos e cancelados
+df_valido = df[df['status'] == 'ENTREGUE'] #Para calculo de receita
+df_cancelado = df[df['status'] == 'CANCELADO']
+df_devolvido = df[df['status'] == 'DEVOLVIDO']
+
+
 
 print(df.dtypes)
-print(df.tail(20))
+print(df.sample(20))
