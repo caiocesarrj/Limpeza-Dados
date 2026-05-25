d2d9817baae12e6920b59c8a951641c2a766c5ea
Caio <caiocesar0052@gmail.com>
2026-05-25 09:21:29 -0300

    Limpeza no Python Concluída

--- limpeza.py
+++ limpeza.py
@@ -6,15 +6,16 @@ df = pd.read_csv(r'C:\Projetos\Limpeza-Dados\DadosBrutos\pedidos_raw.csv')
 
 df = df.drop_duplicates(subset='id_pedido', keep='first') # Remove duplicatas
 
+#Correção e Remoção de Valores Nulos
+df['cliente_id'] = df['cliente_id'].fillna('CLI-N/I')
+df['avaliacao'] = df['avaliacao'].fillna(0)
+df = df.dropna(subset=['quantidade', 'preco_unitario']) #Removendo quantidades e precos nulos, não tem valor de receita
+
+
 #Conversões
 df['quantidade'] = pd.to_numeric(df['quantidade'], errors='coerce').astype('Int64') # Quantidade de Float para Int
 df['data_pedido'] = pd.to_datetime(df['data_pedido'], format='mixed', errors='coerce') # Convertendo Data de STR pra Date e ordenando
 
-#Correção e Remoção de Valores Nulos
-df['cliente_id'] = df['cliente_id'].fillna('CLI-N/I')
-df['email'] = df['email'].fillna('Email não informado')
-df['avaliacao'] = df['avaliacao'].fillna(0)
-df = df.dropna(subset=['quantidade', 'preco_unitario']) #Removendo quantidades e precos nulos, não tem valor de receita
 
 #Correção nos dados
 df['nome_cliente'] = df['nome_cliente'].str.title() # Padronizando nomes
@@ -28,6 +29,8 @@ df['status'] = df['status'].str.upper()
 
 df.loc[df['desconto'] > 1, 'desconto'] = np.nan # Descontos acima de 100% retornam desconhecido
 
+df['preco_unitario'] = df['preco_unitario'].abs() # Valores negativos para positivos
+
 
 #Receita de pedidos entregues, devolvidos e cancelados
 df_valido = df[df['status'] == 'ENTREGUE'] #Para calculo de receita
@@ -36,5 +39,8 @@ df_devolvido = df[df['status'] == 'DEVOLVIDO']
 
 
 
-print(df.dtypes)
-print(df.sample(20))
+print(df.head(10))
+
+df.to_csv(r'C:\Projetos\Limpeza-Dados\Pedidos_Limpos.csv', index = False, encoding='utf-8')
+df_check = pd.read_csv(r'C:\Projetos\Limpeza-Dados\Pedidos_Limpos.csv')
+print('Df salvo com sucesso')
